f2df112013e17f5d56ad687748ecaa0d8c752941
Steve Robinson <steve.rob@me.com>
2016-10-22 14:24:30 +0530

    diff parsing gems

--- testing.rb
+++ testing.rb
@@ -1,4 +1,9 @@
-#FIXME - Hello
-def hello_world
+#FIXME - Hello World Changed to hello there
+def hello_there
+  puts 'Hello, World!'
+end
+
+#TODO - new comment
+def hello_there
   puts 'Hello, World!'
 end
