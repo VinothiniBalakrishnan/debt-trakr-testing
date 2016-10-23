293b6015023b11afcccb9d3fd534505cc929705f
Vinothini <vinothinibj@gmail.com>
2016-10-23 18:19:36 +0530

    test commit

--- testing.rb
+++ testing.rb
@@ -1,16 +1,4 @@
-#FIXME - Hello World Changed to hello there
-def hello_there
+#FIXME - Need to fix this method
+def my_hello_there
   puts 'Hello, World!'
 end
-
-def hello_there
-  puts 'Hello, World --'
-end
-
-
-#FIXME - branding nrew comment added
-def hello_there
-  puts 'Hello, World --'
-end
-
-#TODO - new comment
